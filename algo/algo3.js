f8ad7a9d1525887092d90dddbb4114c6933b92e6
samson-corbiere <samson.corbiere@gmail.com>
2020-10-16 10:20:44 +0200

    algo3 terminé

--- algo/algo3.js
+++ algo/algo3.js
@@ -14,9 +14,18 @@ Au final la liste devra ressembler à :
 26-99
 26-100
 */
+const colonne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26] 
 
+const list = [];
 function theaterSieges() {
+  for (i =1; i <= colonne.length; i++) {
+    for (j= 1; j <= 100; j++) {
+      list.push(`${i}-${j}`)
+    }
+  }
+  return list;
   // Your code here !
 }
 
 console.log(theaterSieges());
+
